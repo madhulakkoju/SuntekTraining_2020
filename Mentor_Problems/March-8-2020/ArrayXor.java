2c0aba4cca607ee5ff7104514be833f4d332922b
Madhu Lakkoju <53974577+madhulakkoju@users.noreply.github.com>
2020-03-09 20:06:29 +0530

    Update ArrayXor.java

--- Mentor_Problems/March-8-2020/ArrayXor.java
+++ Mentor_Problems/March-8-2020/ArrayXor.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 /*
-QUESTION 
+QUESTION  - 1
 Given the array arr of positive integers and the array queries where queries[i] = [Li, Ri], for
 each query i compute the XOR of elements from Li to Ri (that is, arr[Li] xor arr[Li+1] xor ... xor
 arr[Ri] ). Return an array containing the result for the given queries.
